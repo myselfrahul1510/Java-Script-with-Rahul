41d98d66dc879c309aeeed2c1ff941944ddbeaff
myselfrahul1510 <iamrahul1401@gmail.com>
2024-08-18 08:38:34 +0000

    Variables Done

---
+++ 02_Variable.js
@@ -0,0 +1,16 @@
+const accoutId = 144553
+let accountEmail = "nfvjnd@jfdvd.com"
+var accountPassword = "151515"          // Don't prefer to use 'var' keyword because this keyword has issue in block scope and functional scope.
+accountCity = "Kolkata"                 // This is also not preferable, java script is high profile language so it is provide this type advantage.
+let accountstate
+
+console.table([accountEmail,accountPassword,accountCity,accountstate])
+
+// accoutId = 253214          // this is not allowed
+accountEmail = "sjdbchj@bsbhc.com"
+accountPassword = "55484"
+accountCity = "Payradanga"
+
+console.log(accoutId);        // ";" this is not important in Java Script, if you use or not this is not matter
+
+console.table([accountEmail,accountPassword,accountCity])
