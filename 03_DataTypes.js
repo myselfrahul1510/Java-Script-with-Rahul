ac6865b4924e7d09aba1918a733e3cb181d575e4
myselfrahul1510 <iamrahul1401@gmail.com>
2024-08-18 08:39:10 +0000

    Data Types Done

---
+++ 03_DataTypes.js
@@ -0,0 +1,30 @@
+"use strict";                 //treat all JS Code as newer version
+//alert(3 + 3)                //we are using nodejs, not browser
+
+console.log(23+3);            //code readibility should be high
+
+console.log("Rahul");
+
+
+
+
+let name = "Rahul"
+let age = 19
+let isLoggedIn = true
+
+
+/* number => 2 to the power 53
+bigint => very very large number we use it
+string => use in double cote "" or single cote ''
+boolean => true or false
+Null => standalone value
+undefined => the value is assign now
+symbol => unique
+object => 
+*/
+
+console.log(typeof "Rahul");
+console.log(typeof Symbol);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
